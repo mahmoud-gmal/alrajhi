30c816ecb8bf2c789516fcaa29f0ed613e50ebc0
himamohamed1991 <himamohamed1991@gmail.com>
2022-06-27 11:37:48 +0200

    add_header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,17 +1,33 @@
-import Image from "next/image";
-
+import Head from 'next/head'
+import { useRouter } from 'next/router' 
 import Link from "next/link";
-import { useRouter } from 'next/router';
-
-
-const Header = () => {
-
+ 
+function Header({ title, keywords, description, children }) {
+  const router = useRouter()
 
   return (
-    <>
-    HEADER
-    </>
-  );
-};
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+      </Head>
+
+      <Header />
+
+      {/* {router.pathname === '/' && <Showcase />} */}
+
+      <div className={styles.container}>{children}</div>
+    
+    </div>
+  )
+}
+
+Layout.defaultProps = {
+  title: 'ALragihi property',
+  description: 'Find the latest properties',
+  keywords: 'rents, sell, buy, events',
+}
+
 
 export default Header;
